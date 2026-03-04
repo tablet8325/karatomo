13539026176712e89c1545a30959b595a79526db
tablet8325 <tablet8325@gmail.com>
2026-03-04 19:17:07 +0900

    Update MainActivity.kt

--- app/src/main/java/org/karatomo/app/MainActivity.kt
+++ app/src/main/java/org/karatomo/app/MainActivity.kt
@@ -1,22 +1,17 @@
 package org.karatomo.app
 
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import org.karatomo.app.ui.NewSongFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        try {
-            setContentView(R.layout.activity_main)
-            if (savedInstanceState == null) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, NewSongFragment())
-                    .commit()
-            }
-        } catch (e: Exception) {
-            Toast.makeText(this, "초기화 에러: ${e.message}", Toast.LENGTH_LONG).show()
-        }
+        
+        // 레이아웃 파일을 부르지 않고, 코드로 직접 글자를 띄워봅니다.
+        // 이렇게 해서 앱이 켜진다면 '레이아웃 XML'이나 '프래그먼트'가 범인입니다.
+        val tv = TextView(this)
+        tv.text = "메인 화면 접속 성공! 프래그먼트 로딩 전입니다."
+        setContentView(tv)
     }
 }
