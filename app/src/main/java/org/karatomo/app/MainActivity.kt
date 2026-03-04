a5a9926182bce6584107862e9ede8e40ea5e5af4
tablet8325 <tablet8325@gmail.com>
2026-03-04 19:11:26 +0900

    Update MainActivity.kt

--- app/src/main/java/org/karatomo/app/MainActivity.kt
+++ app/src/main/java/org/karatomo/app/MainActivity.kt
@@ -1,16 +1,22 @@
 package org.karatomo.app
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.karatomo.app.ui.NewSongFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, NewSongFragment())
-            .commit()
+        try {
+            setContentView(R.layout.activity_main)
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, NewSongFragment())
+                    .commit()
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this, "초기화 에러: ${e.message}", Toast.LENGTH_LONG).show()
+        }
     }
 }
