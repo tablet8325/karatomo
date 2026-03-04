26da0452f3d6d4e837e4ce71e22fc5ff44b7b58a
tablet8325 <tablet8325@gmail.com>
2026-03-04 18:45:42 +0900

    Create PlaylistAdapter.kt

---
+++ app/src/main/java/org/karatomo/app/ui/PlaylistAdapter.kt
@@ -0,0 +1,30 @@
+package org.karatomo.app.ui
+
+import android.view.*
+import androidx.recyclerview.widget.RecyclerView
+import org.karatomo.app.databinding.ItemPlaylistBinding
+import org.karatomo.app.model.Playlist
+
+class PlaylistAdapter(
+    private val playlists: List<Playlist>
+) : RecyclerView.Adapter<PlaylistAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: ItemPlaylistBinding)
+        : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemPlaylistBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val playlist = playlists[position]
+        holder.binding.tvName.text = "${playlist.name} (${playlist.songs.size})"
+    }
+
+    override fun getItemCount() = playlists.size
+}
