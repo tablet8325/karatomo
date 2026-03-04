d60ab83d869c90fae92f4c4e42f83166920be251
tablet8325 <tablet8325@gmail.com>
2026-03-04 20:00:12 +0900

    Update Song.kt

--- app/src/main/java/org/karatomo/app/network/Song.kt
+++ app/src/main/java/org/karatomo/app/network/Song.kt
@@ -1,11 +1,13 @@
 package org.karatomo.app.network
 
+import com.google.gson.annotations.SerializedName
+
 data class Song(
-    val brand: String,
-    val no: String,
-    val title: String,
-    val singer: String,
-    val composer: String? = null,
-    val lyricist: String? = null,
-    val release: String? = null
+    @SerializedName("brand") val brand: String? = null,
+    @SerializedName("no") val no: String? = null,
+    @SerializedName("title") val title: String? = null,
+    @SerializedName("singer") val singer: String? = null,
+    @SerializedName("composer") val composer: String? = null,
+    @SerializedName("lyricist") val lyricist: String? = null,
+    @SerializedName("release") val release: String? = null
 )
