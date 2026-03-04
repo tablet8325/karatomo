5d87701f7d1d2e1c731b492c7d95ca39f8bb426e
tablet8325 <tablet8325@gmail.com>
2026-03-04 17:18:47 +0900

    Create KaraokeApi.kt

---
+++ app/src/main/java/org/karatomo/app/network/KaraokeApi.kt
@@ -0,0 +1,30 @@
+package org.karatomo.app.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface KaraokeApiService {
+    @GET("karaoke.json")
+    suspend fun getSongs(@Query("brand") brand: String): List<Song>
+}
+
+object KaraokeApi {
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    val service: KaraokeApiService = Retrofit.Builder()
+        .baseUrl("https://api.manana.kr/")
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(KaraokeApiService::class.java)
+}
