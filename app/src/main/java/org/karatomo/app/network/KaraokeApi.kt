6cefde7450ac207cec0568280cd5761b4afb2591
tablet8325 <tablet8325@gmail.com>
2026-03-06 22:39:46 +0900

    Update KaraokeApi.kt

--- app/src/main/java/org/karatomo/app/network/KaraokeApi.kt
+++ app/src/main/java/org/karatomo/app/network/KaraokeApi.kt
@@ -1,6 +1,5 @@
 package org.karatomo.app.network
 
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
@@ -12,11 +11,11 @@ interface KaraokeService {
         "Accept: application/json"
     )
     @GET("release")
-    fun getReleaseSongs(@Query("searchmonth") month: String, @Query("brand") brand: String): Call<List<Song>>
+    fun getReleaseSongs(@Query("searchmonth") month: String, @Query("brand") brand: String): retrofit2.Call<List<Song>>
 
     @Headers("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
     @GET("search")
-    fun searchSongs(@Query("query") query: String, @Query("brand") brand: String, @Query("type") type: String): Call<List<Song>>
+    fun searchSongs(@Query("query") query: String, @Query("brand") brand: String, @Query("type") type: String): retrofit2.Call<List<Song>>
 }
 
 object KaraokeApi {
